7d6e8406b79258f2de8860fbb44ccdf3d31e9af3
Romain Calascibetta <romain.calascibetta@gmail.com>
2021-07-10 13:29:04 +0200

    Add an interface for IPC

---
+++ ipc/ipc.mli
@@ -0,0 +1,9 @@
+type t
+
+val is_empty : t -> bool
+val dequeue : t -> int64
+val enqueue : t -> int64 -> unit
+val connect : string -> t
+val close : t -> unit
+
+val create : string -> (unit, [> `Msg of string ]) result
