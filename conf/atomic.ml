8318edf51adf091a6dd55495017ab0257f608f3a
Romain Calascibetta <romain.calascibetta@gmail.com>
2022-07-20 13:14:58 +0200

    Fix the compilation for OCaml 5.0

--- conf/atomic.ml
+++ conf/atomic.ml
@@ -2,6 +2,6 @@ let parse s = Scanf.sscanf s "%d.%d" (fun major minor -> (major, minor))
 
 let () =
   let version = parse Sys.ocaml_version in
-  if version >= (4, 12)
+  if version >= (4, 12) && version < (5, 0)
   then print_string "atomic_stdlib.ml"
   else print_string "atomic_pre412.ml"
